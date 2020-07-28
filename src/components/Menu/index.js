2ec214f77701b35b420e0c5e6ec01dd5df5ac1b8
Maria Carolina <carol-martins7@hotmail.com>
2020-07-27 21:57:04 -0300

    criação da página home utilizando conceitos de componentização

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+// import ButtonLink from './components/ButtonLink';
+import Button from '../Button/'
+import './Menu.css';
+
+function Menu(){
+  return(
+    <nav className="Menu">
+      <a href="/">
+        <img className='Logo' src={Logo} alt="AluraFlix" />
+      </a>
+      <Button as="a" className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
